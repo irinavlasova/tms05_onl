ee818a41da6721c556dd82104dfe9f14688eb9a1
Alesia Kapashilova <alesia.andreevna94@gmail.com>
2021-12-24 16:09:16 +0300

    fixed typo

--- alesia_kapashilova/hm3/task_3_2.py
+++ alesia_kapashilova/hm3/task_3_2.py
@@ -1,6 +1,6 @@
 # Напишите программу, которая добавляет ‘ing’ к словам
 word = ['read', 'meet', 'sing', 'eat', 'driv']
-new_word= []
+new_word = []
 for element in word:
     new_word.append(element + 'ing')
 print(new_word)
