281e41b3cce7b36616ba30e5b7bb4d6895f4cacf
nik-ar <96441363+nik-ar@users.noreply.github.com>
2021-12-26 12:32:37 +0300

    Delete veronika_ivashko/hw_3 directory

--- veronika_ivashko/hw_3/task_3_1.py
+++
@@ -1,6 +0,0 @@
-"""
-1. Заменить символ “#” на символ “/” в строке 'www.my_site.com#about
-"""
-
-some_string = 'www.my_site.com#about'
-print(some_string.replace('#', '/'))
--- veronika_ivashko/hw_3/task_3_3.py
+++
@@ -1,9 +0,0 @@
-"""
-3. В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
-"""
-
-some_string = 'Ivanou Ivan'
-some_string_splited = some_string.split()[::-1]
-result = ' '.join(some_string_splited)
-
-print(f'String "{some_string}" is transformed into "{result}".')
